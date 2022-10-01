6b77d7c4bdc5de92cc7df9918c74cba92d6b8f25
Dante <dantemmccray@gmail.com>
2022-09-30 21:50:38 -0700

    Function HW

---
+++ functions.py
@@ -0,0 +1,16 @@
+
+# Exercise 1 
+#  For example: Say your input parameter to the function is [1,11,14,5,8,9]...Your output should [1,5,8,9]</p>
+# Use the following list - [1,11,14,5,8,9]
+
+l_1 = [1,11,14,5,8,9]
+def range():
+    if l_1 <= 10 .remove:
+        print(l_1)
+
+
+# Question 2
+l_1 = [1,2,3,4,5,6]
+l_2 = [3,4,5,6,7,8,10]
+
+print(l_1 + l_2).sort
